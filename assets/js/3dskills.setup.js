d2eb28f991eb40412f317a6a67c246653dd8dd34
alvaroesalvaroo <aruizgarcia14@uoc.edu>
2026-03-04 21:14:46 +0100

    still workinnn

--- assets/js/3dskills.setup.js
+++ assets/js/3dskills.setup.js
@@ -8,11 +8,16 @@ import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js'
 const modelPath = "./assets/3Dmodels/laboratorio.glb";
 
 const scene = new THREE.Scene();
-const container = document.querySelector(".extra-webgl-container");
+window.scene = scene;
+// const container = window;
+const container = document.querySelector('.extra-webgl-container');
+console.log(container);
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: container.clientWidth,
+    height: container.clientWidth,
 };
+
+console.log("container sizes are " + sizes.width + ", " + sizes.height);
 
 // Create renderer in html canvas webgl element
 const canvas = document.querySelector(".extra-webgl-container canvas");
@@ -46,11 +51,27 @@ function debugModelMatsAndTextures(model)
 
 }
 
-// TODO:
-function setupLight(light) {
-    console.log(`Luz detectada: ${light.name} | Intensidad original: ${light.intensity}`);
-    light.intensity = 10;
+
+const lights = [];
+
+function setupLights() {
+    // Ajustar luz ambiente
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1.3); // Luz general, suave
+    scene.add(ambientLight);
+
+    const sunLight = new THREE.DirectionalLight(0xffffff, 1.2);
+    sunLight.rotation.set(5, 10, 7.5); // Orientación del sol
+    // sunLight.castShadow = true;
+    sunLight.shadow.mapSize.width = 1024;
+    sunLight.shadow.mapSize.height = 1024;
+    sunLight.shadow.camera.near = 0.5;
+    sunLight.shadow.camera.far = 50;
+
+    for (const light of lights) {
+        light.intensity = 10;
+    }
 }
+
 function onScenelLoaded(model)
 {
     scene.add( model );
@@ -61,58 +82,39 @@ function onScenelLoaded(model)
             // console.log("Mesh loaded: " + child.name);
             // debugModelMatsAndTextures(model);
         } else if (child.isLight) {
-            setupLight(child);
+            // Light adjustement
+            lights.push(child);
         }
 
-        if (child.name.includes("CameraPosition1") || child.name.includes("CameraPosition2")) {
-            console.log("Append target");
+        if (child.name.startsWith("CameraPosition")) {
+            console.log("cam position found");
             camPositions.push(child);
         }
         if (child.name.includes("CameraLookAt"))
         {
             console.log("Found camera look at");
 
-            camTarget = child;
+            // camTarget = child;
         }
 
     })
+
+    setupLights();
     console.log("Scene loaded: " + modelPath + " with mesh children: " + childCount);
+
+
+
 }
 
-function createLights()
-{
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.3); // Luz general, suave
-    scene.add(ambientLight);
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(-2, 0, -5);
-
-    scene.add(directionalLight);
-    scene.add(directionalLight.target);
-}
-
-function moveCameraToBlenderPosition() {
-    scene.traverse( ( child ) => {
-        if (child.name === "CameraPosition") {
-            child.updateWorldMatrix(true, false);
-            const worldPos = new THREE.Vector3();
-            console.log(worldPos);
-            child.getWorldPosition(worldPos);
-            camera.position.copy(worldPos);
-            // camera.rotation.copy(child.rotation);
-            // camera.updateProjectionMatrix();
-        }
-    })
-}
-
+// ---------
 // ---------
 // SCREEN RESIZE
 // --------
 window.addEventListener("resize", () => {
     // Update sizes
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
+    sizes.width = container.clientWidth;
+    sizes.height = container.clientHeight;
 
     camera.aspect = sizes.width / sizes.height;
     camera.updateProjectionMatrix();
@@ -137,43 +139,54 @@ const maxMoveSpeed = 1;
 const minMoveSpeed = 0.2;
 let currentTargetIndex = 0;
 let camPositions = [];
-let camTarget;
+// let camTarget;
 
 
 let initialRotationY= null;
 
 let angleBounds = [0.5, -0.5];
 
-function calibrateCameraPosition() {
-    // Initial camera "hardcoded" calibration to refine blender's position in responsive
 
-    // Works ok for mobile
-    // camPositions[0].position.y -= 1;
-    // camPositions[1].position.x += 0.1;
-
+function setupButtons() {
+    let button1 = document.querySelector("#cambutton1");
+    button1.addEventListener("click", function () {
+        console.log("Click on butt1");
+        currentTargetIndex = 0;
+    })
+    let button2 = document.querySelector("#cambutton2");
+    button2.addEventListener("click", function () {
+        currentTargetIndex = 1;
+    })
+    let button3 = document.querySelector("#cambutton3");
+    button3.addEventListener("click", function () {
+        currentTargetIndex = 2;
+    })
+    let button4 = document.querySelector("#cambutton4");
+    button4.addEventListener("click", function () {
+        currentTargetIndex = 3;
+    })
 }
+
+
 function init() {
     renderer.setSize( sizes.width, sizes.height );
-
-
-
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
     // Load glb model
     const loader = new GLTFLoader();
     // Onload
     loader.load( modelPath, function ( gltf ) {
         onScenelLoaded(gltf.scene);
-        // createLights();
-       // moveCameraToBlenderPosition();
-
-        calibrateCameraPosition();
+        setupButtons();
 
         camera.position.copy(camPositions[0].position);
-
-        camera.lookAt(camTarget.position);
+        camera.rotation.copy(camPositions[0].rotation);
+        console.log("cam move to ", camPositions[0].position);
+        camera.lookAt(0, 0, 0);
+        // console.log("cam look at ", camTarget.position);
        // initialRotationY = camera.rotation.y;
         renderer.setAnimationLoop( animate );
-
 
     }, undefined, function ( error ) {
         console.error( error );
@@ -182,46 +195,51 @@ function init() {
 
 // -------------
 // MAIN LOOP
-// ---------------
+// Constants are create outside for a better optimization
 
 const clock = new THREE.Clock();
 
 const targetPos = new THREE.Vector3();
 const targetQuat = new THREE.Quaternion();
+const m1 = new THREE.Matrix4();
+const correction = new THREE.Matrix4().makeRotationX(-Math.PI / 2);
+
+
 
 function animate() {
     const deltaTime = clock.getDelta();
 
     if (camPositions.length === 0) console.log("No cam targets!");
-    const activeTarget = camPositions[currentTargetIndex];
 
+    const activeTarget = camPositions[currentTargetIndex];
     activeTarget.updateWorldMatrix(true, false);
     activeTarget.getWorldPosition(targetPos);
-    activeTarget.getWorldQuaternion(targetQuat);
-
-
-
+    // camera.updateWorldMatrix();
     // camera.quaternion.slerp(targetQuat, rotationSpeed * deltaTime);    // Render
-    camera.lookAt(camTarget.position);
+    // camera.lookAt(camTarget.position);
 
     camera.updateProjectionMatrix();
 
     const distance = camera.position.distanceTo(targetPos);
     let speed = maxMoveSpeed;
-    if (distance < 1)
+    // Cutre lerp
+    let lerpDistance = 1;
+    if (distance < lerpDistance) // Adjust speed when close to objetive
     {
-        speed = maxMoveSpeed *distance/1;
+        speed = maxMoveSpeed * distance / lerpDistance;
+        if (speed < minMoveSpeed) speed = minMoveSpeed;
     }
-    if (distance < 0.1) {
-        speed = minMoveSpeed;
-        currentTargetIndex = (currentTargetIndex + 1) % camPositions.length;
-        console.log("Objetivo alcanzado. Cambiando a:", camPositions[currentTargetIndex].name);
-    }
-    else
-    {
+    if (distance > 0.1) { // Move only when there is threshold to the objetive
         const direction = new THREE.Vector3().subVectors(targetPos, camera.position).normalize();
         camera.position.add(direction.multiplyScalar(speed * deltaTime));
     }
+
+    // Sustituyamos     activeTarget.getWorldQuaternion(targetQuat); por este follon:
+    m1.extractRotation(activeTarget.matrixWorld);
+    m1.multiply(correction);
+    targetQuat.setFromRotationMatrix(m1);
+
+    camera.quaternion.slerp(targetQuat, rotationSpeed * deltaTime);
 
     renderer.render( scene, camera );
 
